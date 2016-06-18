88ddf610b339401273f09ad5ad6babb7d4869352
chiel <michiel.pleijte@gmail.com>
2016-06-18 22:41:50 +0200

    changed init, asks to overwrite .vimrc before proceding.

--- init.sh
+++ init.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-mv ~/.vim ~/.BAKvim$(date "+%Y-%m-%d %H.%M.%S")
-ln -s ~/.vim/vimrc ~/.vimrc
+COMMAND="ln -s $HOME/.vim/vimrc $HOME/.vimrc"
+
+if [ -f "$HOME/.vimrc" ]
+then
+  read -p "File ~/.vimrc already exists, overwrite? (y/n) " choice
+  case "$choice" in 
+    y|Y )
+          echo "Trying it now"
+          rm "${HOME}/.vimrc"
+          `$COMMAND`
+          ;;
+    n|N ) echo "nothing chamged, quit program";;
+    * ) echo "invalid";;
+  esac
+else
+   `$COMMAND`
+fi
